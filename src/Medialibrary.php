e7f612fea7753a213544c369be15e5fc27e8c0b2
Anup <themightysapien@gmail.com>
2023-04-03 00:19:53 +0545

    Clear method and open alias method added

--- src/Medialibrary.php
+++ src/Medialibrary.php
@@ -6,7 +6,17 @@ use Themightysapien\Medialibrary\Models\Library;
 
 class Medialibrary
 {
-    public function init(){
+    public function init()
+    {
         return Library::firstOrNew([]);
+    }
+
+    public function open(){
+        return $this->init();
+    }
+
+    public function clear()
+    {
+        return $this->init()->clearCollection();
     }
 }
