00e15f23a69a45c2558fa9666d39be1ccd400bb1
Anup <themightysapien@gmail.com>
2023-04-03 00:18:08 +0545

    Optional filters and pagination count added

--- config/config.php
+++ config/config.php
@@ -8,4 +8,10 @@ return [
     'collection_name' => 'library',
     'route_prefix' => '/api/medialibrary',
     'route_middleware' => ['api'],
+    'filters' => [
+        \Themightysapien\Medialibrary\Filters\FileNameFilter::class,
+        \Themightysapien\Medialibrary\Filters\FileTypeFilter::class,
+        \Themightysapien\Medialibrary\Filters\DefaultSortFilter::class,
+    ],
+    'items_per_page' => 50
 ];
